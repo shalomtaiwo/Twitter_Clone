5d276dc05c0265be9a9dc82399f3c8d5e3da0d63
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2022-06-16 13:17:24 +0200

    twitter events

--- src/script.ts
+++ src/script.ts
@@ -5,10 +5,8 @@ class twitterApp {
         this.twitter = JSON.parse(localStorage.getItem("tweets") || '[]');
     }
 
-    addNewTweet(lightBoxId,closeLightBoxId,postId,postText, postImage, postLike, postComment,postShare){
+    addNewTweet(postId,postText, postImage, postLike, postComment,postShare){
         this.newTweet ={
-            lightBoxId,
-            closeLightBoxId,
             postId,
             postText,
             postImage,
@@ -31,11 +29,6 @@ class tweetPost extends twitterApp {
         return 'post' + Math.random().toString(36).substring(2, 12);
     }
 
-    lightBoxId(){
-        // I couldn't get how to use Cuid/Uuid with typeScript so I generated random postIds instead
-        return 'lightBox' + Math.random().toString(36).substring(2, 12);
-    }
-
     closeLightBoxId(){
         // I couldn't get how to use Cuid/Uuid with typeScript so I generated random postIds instead
         return 'close' + Math.random().toString(36).substring(2, 12);
@@ -53,9 +46,6 @@ class eventListeners extends tweetPost {
     imageData;
     tweetPost;
     imgInput;
-    lightBox;
-    closeLightBox;
-    lightBoxImage;
     constructor() {
         super();
         this.textPost = document.querySelector("#textPost");
@@ -166,80 +156,6 @@ class eventListeners extends tweetPost {
                             </div>
                         </div>
                         </div>
-                //         <div class="lightBox" id="${newTweet.lightBoxId}">
-                // <div class="lightBoxBody">
-                //     <div class="lightBoxMainHeader">
-                //         <svg id="${newTweet.closeLightBoxId}" viewBox="0 0 24 24" class="closeLightBox"><g><path d="M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z"></path></g></svg>
-                //         <svg viewBox="0 0 24 24" class=" "><g><path d="M21.185 11.47l-9.25-9.25c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l8.72 8.72-8.72 8.72c-.294.294-.294.77 0 1.062.145.146.337.22.53.22s.383-.073.53-.22l9.25-9.25c.293-.295.293-.77 0-1.062z"></path><path d="M13.376 11.47l-9.25-9.25c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l8.72 8.72-8.72 8.72c-.294.294-.294.77 0 1.062.146.145.337.218.53.218s.384-.073.53-.22l9.25-9.25c.293-.293.293-.767 0-1.06z"></path></g></svg>
-                //     </div>
-                //     <div class="lightBoxImage">
-                //         <div class="lightBoxImgSize">
-                //         <img src="${newTweet.postImage}">
-                //         </div>
-                //         <div class="post_reaction">
-                //             <div class="reaction">
-                //                 <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g></svg>
-                //                 <p class="tweetComments"></p>
-                //             </div>
-                //             <div class="reaction">
-                //                 <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path></g></svg>
-                //                 <p class="reTweet"></p>
-                //             </div>
-                //             <div class="reaction">
-                //                 <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
-                //                 <p class="tweetLovePost"></p>
-                //             </div>
-                //             <div class="reaction">
-                //                 <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"></path><path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"></path></g></svg>
-                //                 <p class="tweetSharePost"></p>
-                //             </div>
-                //         </div>
-                //     </div>
-                // </div>
-                //     <div class="lightBoxSideBar">
-                //             <div class="latest_tweets">
-                //             <div class="latest_tweet">
-                //             <div class="lightBoxTweetHeader">
-                //             <div class="post_tweet_user">
-                //                 <img alt="" draggable="true" src="https://pbs.twimg.com/profile_images/1485519634086432775/0cwAi0u1_normal.jpg">
-                //                 <div class="user_post_header">
-                //                     <span>Shalom Taiwo</span>
-                //                     <span class="other_name">@taiwo_shalom</span>
-                //                     </div> 
-                //             </div>
-                //             <div class="post_more">
-                //                 <svg viewBox="0 0 24 24" aria-hidden="true"><g><circle cx="5" cy="12" r="2"></circle><circle cx="12" cy="12" r="2"></circle><circle cx="19" cy="12" r="2"></circle></g></svg>
-                //             </div>   
-                //             </div>
-                //             <div class="latest_post_content">
-                //                 <div class="post_content">
-                //                     <p>${newTweet.postText}</p>
-                //                 </div>
-                //                 <hr>
-                //                 <div class="post_reaction">
-                //                     <div class="reaction">
-                //                         <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z"></path></g></svg>
-                //                         <p class="tweetComments"></p>
-                //                     </div>
-                //                     <div class="reaction">
-                //                         <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M23.77 15.67c-.292-.293-.767-.293-1.06 0l-2.22 2.22V7.65c0-2.068-1.683-3.75-3.75-3.75h-5.85c-.414 0-.75.336-.75.75s.336.75.75.75h5.85c1.24 0 2.25 1.01 2.25 2.25v10.24l-2.22-2.22c-.293-.293-.768-.293-1.06 0s-.294.768 0 1.06l3.5 3.5c.145.147.337.22.53.22s.383-.072.53-.22l3.5-3.5c.294-.292.294-.767 0-1.06zm-10.66 3.28H7.26c-1.24 0-2.25-1.01-2.25-2.25V6.46l2.22 2.22c.148.147.34.22.532.22s.384-.073.53-.22c.293-.293.293-.768 0-1.06l-3.5-3.5c-.293-.294-.768-.294-1.06 0l-3.5 3.5c-.294.292-.294.767 0 1.06s.767.293 1.06 0l2.22-2.22V16.7c0 2.068 1.683 3.75 3.75 3.75h5.85c.414 0 .75-.336.75-.75s-.337-.75-.75-.75z"></path></g></svg>
-                //                         <p class="reTweet"></p>
-                //                     </div>
-                //                     <div class="reaction">
-                //                         <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M12 21.638h-.014C9.403 21.59 1.95 14.856 1.95 8.478c0-3.064 2.525-5.754 5.403-5.754 2.29 0 3.83 1.58 4.646 2.73.814-1.148 2.354-2.73 4.645-2.73 2.88 0 5.404 2.69 5.404 5.755 0 6.376-7.454 13.11-10.037 13.157H12zM7.354 4.225c-2.08 0-3.903 1.988-3.903 4.255 0 5.74 7.034 11.596 8.55 11.658 1.518-.062 8.55-5.917 8.55-11.658 0-2.267-1.823-4.255-3.903-4.255-2.528 0-3.94 2.936-3.952 2.965-.23.562-1.156.562-1.387 0-.014-.03-1.425-2.965-3.954-2.965z"></path></g></svg>
-                //                         <p class="tweetLovePost"></p>
-                //                     </div>
-                //                     <div class="reaction">
-                //                         <svg viewBox="0 0 24 24" aria-hidden="true" ><g><path d="M17.53 7.47l-5-5c-.293-.293-.768-.293-1.06 0l-5 5c-.294.293-.294.768 0 1.06s.767.294 1.06 0l3.72-3.72V15c0 .414.336.75.75.75s.75-.336.75-.75V4.81l3.72 3.72c.146.147.338.22.53.22s.384-.072.53-.22c.293-.293.293-.767 0-1.06z"></path><path d="M19.708 21.944H4.292C3.028 21.944 2 20.916 2 19.652V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V14c0-.414.336-.75.75-.75s.75.336.75.75v5.652c0 1.264-1.028 2.292-2.292 2.292z"></path></g></svg>
-                //                         <p class="tweetSharePost"></p>
-                //                     </div>
-                //                 </div>
-                //                 <hr>
-                //             </div>
-                //         </div>
-                //         </div>
-                //     </div>
-                // </div>
                         `).reverse().join('');
                             }
 
@@ -263,7 +179,7 @@ class eventListeners extends tweetPost {
                 if (this.textPost.value == ''){
                     this.textPost.setAttribute('required','');
                 }else{
-                    this.addNewTweet('','',this.getPostId(),this.textPost.value,'','','','');
+                    this.addNewTweet(this.getPostId(),this.textPost.value,'','','','');
                     this.textOnlyTweet();
                     this.htmlTweet();
                     this.textPost.value = '';
@@ -275,7 +191,7 @@ class eventListeners extends tweetPost {
                 if (this.textPost.value == ''){
                     this.textPost.setAttribute('required','');
                 }else{
-                    this.addNewTweet(this.lightBoxId(),this.closeLightBoxId(),this.getPostId(),this.textPost.value,this.imageData,'','','');
+                    this.addNewTweet(this.getPostId(),this.textPost.value,this.imageData,'','','');
                     this.htmlTweet();
                     this.textPost.value = '';
                     this.tweetImage.value = '';
@@ -285,21 +201,6 @@ class eventListeners extends tweetPost {
             localStorage.setItem("tweets", JSON.stringify(this.twitter));
         })
         }
-
-            
-    documentEvents(){
-        this.closeLightBox = document.querySelector(".closeLightBox");
-        this.lightBox = document.querySelector("#lightBox");
-        this.lightBoxImage = document.querySelector(".getImageLightBox");
-        document.addEventListener("click", (event)=>{
-            this.closeLightBox.addEventListener("click", (event)=>{
-                this.lightBox.style.display = "none";
-            })
-            this.lightBoxImage.addEventListener("click", (event)=>{
-                this.lightBox.style.display = "flex";
-            })
-        })
-    }
 
 
 }
